e4ba724452ef234b9f0c59f17a8821153497ba04
Priyanshu <priyanshu7739410@gmail.com>
2026-05-16 13:10:51 +0000

    base fix

--- app/database/base.py
+++ app/database/base.py
@@ -1,5 +1,3 @@
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql://..."
-
-engine = create_engine(DATABASE_URL)
+Base = declarative_base()
